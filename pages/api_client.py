cfc86b63784f8b4ceb82184300744238b13f49dc
Iana Petrova <yanapetrova@ozon.ru>
2023-07-04 13:32:38 +0300

    bb

---
+++ pages/api_client.py
@@ -0,0 +1,20 @@
+import requests
+from typing import Any
+
+
+class ApiClient:
+    def __init__(self):
+        self.base_url = ''
+        self.cookies = ''
+        self.latest_request = None
+
+    def get(self, path: str, headers: str = None, params: dict[str, Any] = None, time_out: int = 180):
+        self.latest_request = requests.get(url=f'{self.base_url}{path}', headers=headers, params=params, timeout=time_out)
+        self.check_status_code(status_code = 200)
+
+    def check_status_code(self, status_code: int):
+        assert status_code == self.latest_request.status_code, f'Не правильный статус код ожидади {status_code}, получили {self.latest_request.status_code}'
+
+    def post(self, path: str, headers: str = None, params:dict[str, Any] = None, time_out: int = 180, data=None, json=None):
+        self.latest_request = requests.post(url=f'{self.base_url}{path}', headers=headers, params=params, timeout=time_out, data = data, json = json)
+        self.check_status_code(status_code=200)
---
+++ pages/base_page.py
@@ -0,0 +1,69 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class BaseUIMethods:
+
+    def init(self, driver: WebDriver):
+        self.driver = driver
+
+    # получение элемента
+    def element_presence(self, locator: tuple[str, str], driver: WebDriver, message: str,
+                         waiting_time: int = 10) -> WebElement:
+        """
+        Функция на поиск элемента на странице
+        """
+        return WebDriverWait(driver, waiting_time).until(expected_conditions.presence_of_element_located(locator),
+                                                         message=message)
+
+    # получение текста элемента
+    def element_text(self, locator: tuple[str, str], driver: WebDriver, message: str, waiting_time: int = 10):
+        element = self.element_presence(locator, driver, message, waiting_time)
+        return element.get_attribute('innerText')
+
+    # получение значения элемента
+    def get_value(self, locator: tuple[str, str], driver: WebDriver, message: str, waiting_time: int = 10):
+        element = self.element_presence(locator, driver, message, waiting_time)
+        return element.get_attribute('value')
+
+    # получение всех элементов
+    def list_of_element_presence(self, locator: tuple[str, str], driver: WebDriver, message: str,
+                                 waiting_time: int = 10) -> WebElement:
+        return WebDriverWait(driver, waiting_time).until(expected_conditions.presence_of_all_elements_located(locator),
+                                                         message=message)
+
+    # кликать по элементу
+    def click_element(self, locator: tuple[str, str], driver: WebDriver, message: str,
+                      waiting_time: int = 10):
+        self.element_presence(locator, driver, message, waiting_time).click()
+
+    # кликабельный ли элемент
+    def element_to_be_clickable(self, locator: tuple[str, str], driver: WebDriver, message: str,
+                                waiting_time: int = 10):
+        return WebDriverWait(driver, waiting_time).until(expected_conditions.element_to_be_clickable(locator),
+                                                         message=message)
+
+    # не кликательный элемент
+    def element_not_to_be_clickable(self, locator: tuple[str, str], driver: WebDriver, message: str,
+                                    waiting_time: int = 10):
+        return WebDriverWait(driver, waiting_time).until_not(expected_conditions.element_to_be_clickable(locator),
+                                                             message=message)
+
+    # видимость элемента на странице
+    def visibility_of_element_located(self, locator: tuple[str, str], driver: WebDriver, message: str,
+                                      waiting_time: int = 10):
+        return WebDriverWait(driver, waiting_time).until(expected_conditions.visibility_of_element_located(locator),
+                                                         message=message)
+
+    # если элемент откреплен в ДОМ
+    def staleness(self, locator: tuple[str, str], driver: WebDriver, message: str,
+                  waiting_time: int = 10):
+        return WebDriverWait(driver, waiting_time).until(expected_conditions.staleness_of(locator),
+                                                         message=message)
+    #обновление страницы
+    def refresh_page(self):
+        current_url = self.driver.current_url
+        self.driver.refresh()
+        assert current_url == self.driver.current_url
